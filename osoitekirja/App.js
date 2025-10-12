09b8df3a2527280b843a77dfdaf7982cfaabadc3
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-10-12 13:53:33 +0300

    Added firebase realtime database to project

--- osoitekirja/App.js
+++ osoitekirja/App.js
@@ -1,10 +1,16 @@
+import { app } from './firebaseConfig';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { getDatabase, ref, push } from "firebase/database";
 
 export default function App() {
+
+  const database = getDatabase(app);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working something</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ osoitekirja/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaHQdJavD8fvIZLkwiIRsURowES-hrCi4",
+    authDomain: "osoitekirja-aa26d.firebaseapp.com",
+    projectId: "osoitekirja-aa26d",
+    storageBucket: "osoitekirja-aa26d.firebasestorage.app",
+    messagingSenderId: "892221465803",
+    appId: "1:892221465803:web:725a64d1b546a59fc4a74e",
+    measurementId: "G-WNE8JSRQJV"
+};
+
+const app = initializeApp(firebaseConfig);
